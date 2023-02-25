cc8f44891c2f54ab1f80febbc5dc2e14f38a926a
jhennyticona <jhenny.ticona.sis@gmail.com>
2023-02-24 21:34:25 -0300

    calculadora terminada

--- calculadora/Calculadora.java
+++ calculadora/Calculadora.java
@@ -1,10 +1,40 @@
-package src.com.dio.calculadora;
+import java.util.Scanner;
 
 public class Calculadora {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		double a, b;
+		System.out.println("Digite o primeiro valor");
+		a = scan.nextInt();
+		System.out.println("Digite o segundo valor");
+		b = scan.nextInt();
+		
+		double soma = soma(a,b);
+		double subtracao = subtracao(a,b);
+		double divisao = divisao(a,b);
+		double multiplicacao = multiplicacao(a,b);
+		
+		System.out.println("soma: " + soma);
+		System.out.println("subtração: "+ subtracao);
+		System.out.println("divisao: "+ divisao);
+		System.out.println("multiplicação: "+ multiplicacao);
+	}
 
+	public static double soma(double a, double b) {
+		return a + b;
+	}
+
+	public static double subtracao(double a, double b) {
+		return a - b;
+	}
+
+	public static double divisao(double a, double b) {
+		return a / b;
+	}
+
+	public static double multiplicacao(double a, double b) {
+		return a * b;
 	}
 
 }
